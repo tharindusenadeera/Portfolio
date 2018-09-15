45b494b71654e1fa5d1c9f263273ddf5ea1e6de7
tharindusenadeera <tharindusenadeera081@gmail.com>
2018-09-15 23:49:12 +0530

    Adding router

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Layout, Header, Navigation, Drawer, Content, Footer, FooterSection, FooterLinkList } from 'react-mdl';
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
 
 class App extends Component {
     render() {
-        return (<div style={{ height: '150px', position: 'relative' }}>
+        return (<div style={{ height: '300px', position: 'relative' }}>
             <Layout style={{ background: 'url(http://www.getmdl.io/assets/demos/transparent.jpg) center / cover' }}>
                 <Header transparent title="Title" style={{ color: 'white' }}>
                     <Navigation>
@@ -23,16 +23,12 @@ class App extends Component {
                     </Navigation>
                 </Drawer>
                 <Content>
-                    <div className='page-content'></div>
-                </Content>
-                <Footer size="mini">
-                    <FooterSection type="left" logo="Title">
-                        <FooterLinkList>
-                            <a href="/">Help</a>
-                            <a href="/">Privacy & Terms</a>
-                        </FooterLinkList>
-                    </FooterSection>
-                </Footer>
+                    <div className='page-content'>
+                        <p>Example for the Portfolio Website</p>
+                    </div>
+                </Content><br />
+
+
             </Layout>
 
         </div>);
