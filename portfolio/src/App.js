79706890c6e33e767fc6aecacb22fd16f908b699
tharindusenadeera <tharindusenadeera081@gmail.com>
2018-09-16 01:45:41 +0530

    Links

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,26 +2,28 @@ import React, { Component } from 'react';
 import './App.css';
 import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
 import Main from './components/main';
+import { Link } from 'react-router-dom';
 
 
 class App extends Component {
     render() {
         return (<div style={{ height: '300px', position: 'relative' }}>
             <Layout style={{ background: 'url(http://www.getmdl.io/assets/demos/transparent.jpg) center / cover' }}>
-                <Header transparent title="Title" style={{ color: 'white' }}>
+                <Link to="/"><Header transparent title="Portfolio" style={{ color: 'white' }} >
                     <Navigation>
-                        <a href="/">Link</a>
-                        <a href="/">Link</a>
-                        <a href="/">Link</a>
-                        <a href="/">Link</a>
+                        <Link to="/aboutme">About Me</Link>
+                        <Link to="/contact">Contact</Link>
+                        <Link to="/projects">Projects</Link>
+                        <Link to="/resume">Resume</Link>
                     </Navigation>
                 </Header>
+                </Link>
                 <Drawer title="Title">
                     <Navigation>
-                        <a href="/">Link</a>
-                        <a href="/">Link</a>
-                        <a href="/">Link</a>
-                        <a href="/">Link</a>
+                        <Link to="/aboutme">About Me</Link>
+                        <Link to="/contact">Contact</Link>
+                        <Link to="/projects">Projects</Link>
+                        <Link to="/resume">Resume</Link>
                     </Navigation>
                 </Drawer>
                 <Content>
