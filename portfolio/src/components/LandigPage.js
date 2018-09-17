226cac45aff1c6134b4c030887901f1c9fae98ce
tharindusenadeera <tharindusenadeera081@gmail.com>
2018-09-17 10:25:24 +0530

    landing page

--- portfolio/src/components/LandigPage.js
+++ portfolio/src/components/LandigPage.js
@@ -8,7 +8,13 @@ class LandingPage extends Component {
         return (
             <div style={{ width: '100%', margin: 'auto' }}>
                 <Grid className="landing-grid">
-                    <Cell col={12}></Cell>
+                    <Cell col={12}>
+                        <img src="https://cdn0.iconfinder.com/data/icons/user-pictures/100/matureman1-512.png" alt="avatar" className="avatar-img" />
+                        <div className="banner-text"><h1>Full Stack Web Developer</h1>
+                            <hr />
+                            <p>HTML/CSS | JavaScript | React | Angular | Node JS | Express | MySQL</p>
+                        </div>
+                    </Cell>
                 </Grid>
             </div>
         );
