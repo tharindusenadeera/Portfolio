7e65eee1a4205139a3d0bbc33f5d51b4917a2e49
tharindusenadeera <tharindusenadeera081@gmail.com>
2018-09-23 00:59:57 +0530

    Contact page final

--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Grid, Cell } from "react-mdl";
+import { Grid, Cell, List, ListItem, ListItemContent } from "react-mdl";
 
 
 class Contact extends Component {
@@ -17,7 +17,34 @@ class Contact extends Component {
                         <p style={{ width: '75%', margin: 'auto', paddingTop: '1em' }}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged</p>
                     </Cell>
                     <Cell col={6}>
-                        <h1>Second half</h1>
+                        <h2>Contact Me</h2>
+                        <hr />
+                        <div className="contact-list">
+                            <List>
+                                <ListItem>
+                                    <ListItemContent style={{ fontSize: '25px', fontFamily: 'Anton' }}>
+                                        <i className="fa fa-phone-square" aria-hidden="true" />
+                                        (+94) 71 5475 220
+                                    </ListItemContent>
+                                </ListItem>
+
+                                <ListItem>
+                                    <ListItemContent style={{ fontSize: '25px', fontFamily: 'Anton' }}>
+                                        <i className="fa fa-fax" aria-hidden="true" />
+                                        (+94) 33 2281 709
+                                    </ListItemContent>
+                                </ListItem>
+
+                                <ListItem>
+                                    <ListItemContent style={{ fontSize: '25px', fontFamily: 'Anton' }}>
+                                        <i className="fa fa-envelope" aria-hidden="true" />
+                                        tharindusenadeera081@gmail.com
+                                    </ListItemContent>
+                                </ListItem>
+
+                            </List>
+                        </div>
+
                     </Cell>
                 </Grid>
             </div>
